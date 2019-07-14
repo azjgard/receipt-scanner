6f0974d3833562b93cca131a156adb090c2b57ff
Jordin Gardner <gard.jordin@gmail.com>
2019-07-14 02:08:52 -0700

    implement working OCR using free api

--- App.tsx
+++ App.tsx
@@ -8,7 +8,11 @@ import {
     ImageSourcePropType
 } from 'react-native';
 import { Camera } from 'expo-camera';
+import { CapturedPicture } from 'expo-camera/build/Camera.types';
+import axios from 'axios';
+
 import { useCamera, ICamera } from './camera/useCamera';
+import { OCR_API_KEY } from './env';
 
 export default function App() {
     const camera = useCamera();
@@ -28,12 +32,33 @@ function CameraContainer({ camera }: { camera: ICamera }) {
         imagePreview,
         setImagePreview
     ] = useState<ImageSourcePropType | null>(null);
+    const [imageText, setImageText] = useState('');
 
     const setCameraRef = (c: Camera) => (cameraRef = c);
 
     const takePic = async () => {
         if (cameraRef) {
-            cameraRef.takePictureAsync().then(setImagePreview);
+            cameraRef
+                .takePictureAsync({ base64: true, quality: 0.1 })
+                .then(picture => {
+                    setImagePreview(picture);
+                    extractTextFromPicture(picture);
+                });
+        }
+    };
+
+    const extractTextFromPicture = async (picture: CapturedPicture) => {
+        const data = new FormData();
+        data.append('base64Image', 'data:image/jpeg;base64,' + picture.base64!);
+
+        const response = await axios.post(
+            'https://api.ocr.space/parse/image',
+            data,
+            { headers: { apikey: OCR_API_KEY } }
+        );
+
+        if (response.status == 200) {
+            setImageText(response.data.ParsedResults[0].ParsedText);
         }
     };
 
@@ -47,9 +72,13 @@ function CameraContainer({ camera }: { camera: ICamera }) {
             <TouchableOpacity onPress={camera.toggleCameraType}>
                 <Text>Switch Camera</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={takePic}>
+            <TouchableOpacity
+                onPress={takePic}
+                style={{ width: 80, height: 80, backgroundColor: 'red' }}
+            >
                 <Text>Oh SNAP</Text>
             </TouchableOpacity>
+            {!!imageText && <Text>{imageText}</Text>}
             {!!imagePreview && (
                 <Image
                     source={imagePreview}
